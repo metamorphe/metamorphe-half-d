c92791f695fb428582317ee0dc47a67b9b47b3e9
Cesar Torres <cearto@berkeley.edu>
2015-02-12 15:08:09 -0800

    Plane-mapped. Merging changes and branching

--- rails-halfd/app/assets/javascripts/metamorphehalf/planebox.js
+++ rails-halfd/app/assets/javascripts/metamorphehalf/planebox.js
@@ -4,6 +4,7 @@ function PlaneBox(h, w, d, h_s, w_s, d_s){
 	this.depth = d;
 	this.geometry = new THREE.BoxGeometry(h, w, d, h_s, w_s, d_s);
 	this.mesh = new THREE.Mesh(	this.geometry, lambertMaterial);
+	this.material = lambertMaterial;
 	this.mesh.rotation.x = -Math.PI / 2;
 	this.mesh.position.y = -100;
 }
@@ -17,31 +18,27 @@ PlaneBox.prototype = {
 		this.top_indices = [];
 		var self = this;
 		
+		// vertice indices
 		$.map(geom.vertices, function(el, i){
 			if(el.z >= self.depth/2.0) self.top_indices.push(i);
 		});
-		
+
+
+
 		return this.top_indices;
 	}, 
-	applyToTop: function(fn){
+	// applies fn to top vertex indices
+	applyToTop: function(fn, vertices){
 		var top = this.cacheTop();
-		var geom = this.geometry;
 		$.each(top, function(i, el){
-			var vertex = geom.vertices[el]
+			var vertex = vertices[el]
 			fn(vertex);
 		});
 		geom.verticesNeedUpdate = true;
 		return true;
 	}
 
-	// function extractFaces(vertexIndices){
- //  	  //INDEX OF FACES
- //      counts = {};
- //      faces = $.map(vertexIndices, function(e){ return toFaces(e)});//.sort();
- //      $.each(faces, function(i,e){ if(typeof counts[e] === "undefined") counts[e] = 1; else counts[e] ++ });
- //      faces = $.map(counts, function(e, i){ if(e >= 1) return parseInt(i)});
- //      return faces;
-	// }
+
 
 	// function facesToNewRegion(faces){
 	//   data = {faces: faces, pca1: [0, 1, 0], pca2: [1, 0, 0], pca3: [0, 0, 1], neighbors_computed: false};
@@ -49,10 +46,19 @@ PlaneBox.prototype = {
 	//   stl.regions.push(r);
 	//   r.highlight(true);
 	// }
-
-	// function toFaces(p){ 
-	//   return $.map(stl.object.geometry.faces, function(e, i){ if(e.a == p || e.b == p || e.c == p) return i; })
-	// }
-
-
 }
+
+// function extractFaces(geometry, vertexIndices){
+// 		// console.log(geometry, vertexIndices);
+//   	  //INDEX OF FACES
+//       counts = {};
+//       console.log(vertexIndices.length);
+//       faces = $.map(vertexIndices, function(e){ return toFaces(geometry, e)});//.sort();
+//       $.each(faces, function(i,e){ if(typeof counts[e] === "undefined") counts[e] = 1; else counts[e] ++ });
+//       faces = $.map(counts, function(e, i){ if(e >= 1) return parseInt(i)});
+//       return faces;
+// }
+
+// function toFaces(geometry, p){ 
+// 	return $.map(geometry.faces, function(e, i){ if(e.a == p || e.b == p || e.c == p) return i; })
+// }

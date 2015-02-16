eb77b8e75dd96a5d5192ae3cbb2d66b333873d5e
Cesar Torres <cearto@berkeley.edu>
2015-02-16 08:53:09 -0800

    Initial textures to test

--- rails-halfd/app/controllers/threejs_controller.rb
+++ rails-halfd/app/controllers/threejs_controller.rb
@@ -1,6 +1,8 @@
 class ThreejsController < ApplicationController
   def height_displacement
+	@files = Dir.glob("public/heightmap/images/*").collect!{|c| c.split('/')[-1]}.to_json.html_safe
   	render :layout => "full_screen"
+  	# render :json => @files
   end
   def environment
   	render :layout => "full_screen"

42e32597924d8e011079d40260ba4ed3842411d3
Cesar Torres <cearto@berkeley.edu>
2015-03-01 13:52:40 -0800

    Texture maps precomputed before tc load

--- rails-halfd/app/assets/javascripts/metamorphehalf/ShaderBumpBox.js
+++ rails-halfd/app/assets/javascripts/metamorphehalf/ShaderBumpBox.js
@@ -18,7 +18,7 @@ ShaderBumpBox.prototype = {
 	}, 
 	raise: function(mag){
 		this.mag = mag;
-		this.obj.material.uniforms.bumpScale.value = this.mag;
+		// this.obj.material.uniforms.bumpScale.value = this.mag;
 	}
 }
 
@@ -39,11 +39,20 @@ ShaderBumpBox.makeShaderTexture = function(texture, magnitude){
 	var uniforms = THREE.UniformsUtils.clone(phongShader.uniforms);
 	uniforms.bumpTexture = { type: "t", value: bumpTexture };
 	uniforms.bumpScale = { type: "f", value: bumpScale };
-	
-	return new THREE.ShaderMaterial({
+	console.log("Generating..");
+	return new THREE.MeshLambertMaterial({
+			map: bumpTexture,
+			bumpMap: bumpTexture,
+			bumpScale: magnitude,
+			ambient: new THREE.Color( 0xff0000 ),
+	        color: new THREE.Color( 0xffffff ),
+	        specular: new THREE.Color( 0x00ff00 ),
+	        // emissive: new THREE.Color( 0x0000ff ),
+	        side: THREE.DoubleSide,
+	        shininess: 0,
 		    uniforms: uniforms,
-			vertexShader:  phongShader.vertexShader, //document.getElementById( 'vertexShader' ).textContent,
-			fragmentShader: phongShader.fragmentShader, //document.getElementById( 'fragmentShader' ).textContent,
+			// vertexShader:  phongShader.vertexShader, //document.getElementById( 'vertexShader' ).textContent,
+			// fragmentShader: phongShader.fragmentShader, //document.getElementById( 'fragmentShader' ).textContent,
 		  	lights: true,
     		fog: true,
 			side: THREE.DoubleSide

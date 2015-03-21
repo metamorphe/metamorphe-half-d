f8812e9bb4d2992d94901c43045e8d42f6dd34c5
Cesar Torres <cearto@berkeley.edu>
2015-03-20 17:21:39 -0700

    Seeds

--- rails-halfd/db/seeds.rb
+++ rails-halfd/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+layers = Layer.create([{name: "Surface", name: "Structure", name: "Interactive", name: "Weight"}]);
